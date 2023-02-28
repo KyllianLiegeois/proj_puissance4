e61c809ea12ddfb98cacf90c4032333b7ef847ab
Kyllian Liegeois <kyllian.liegeois@e.rascol.net>
2023-02-28 09:59:35 +0100

    test 2

--- test/integration/test_integration1.py
+++ test/integration/test_integration1.py
@@ -13,3 +13,15 @@ def test_exemple():
            [0,  1, -1, -1, -1, -1, -1],
            [0,  1, -1, -1, -1, -1, -1]]
    assert True==True
+
+def test_maj_jeu():
+    grille=[[-1]*7 for _ in range(6)]
+    niveau=[5]*7
+    joueur = 0
+    i, j = maj_jeu('b1', grille, niveau, joueur)
+    assert i == 5 and j == 0 and grille[i][j] == joueur
+    i, j = maj_jeu('b1', grille, niveau, joueur)
+    assert i == 4 and j == 0 and grille[i][j] == joueur
+    i, j = maj_jeu('b1', grille, niveau, joueur)
+    assert i == 3 and j == 0 and grille[i][j] == joueur
+  
